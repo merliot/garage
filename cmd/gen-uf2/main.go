8909d8afc9fda84e910563e6c2b9384c49590261
Scott Feldman <sfeldma@gmail.com>
2024-03-01 11:20:53 -0800

    go generate uf2

--- cmd/gen-uf2/main.go
+++ cmd/gen-uf2/main.go
@@ -7,9 +7,10 @@ import (
 	"github.com/merliot/garage"
 )
 
+//go:generate go run main.go
 func main() {
 	garage := garage.New("proto", "garage", "proto").(*garage.Garage)
-	if err := garage.GenerateUf2s(); err != nil {
+	if err := garage.GenerateUf2s("../.."); err != nil {
 		log.Println("Error generating UF2s:", err)
 		os.Exit(1)
 	}
